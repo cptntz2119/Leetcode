118449692c44a8fd76eeebdaa9bd7b7db459f048
Dong <zhangdong2119@hotmail.com>
2022-05-16 20:40:48 -0700

    Time: 96 ms (66.66%), Space: 50.1 MB (82.25%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,19 +3,34 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-   	// initialize current & max values equal to the value of the first number in nums
-    let current = nums[0];
-    let max = nums[0];   
+//    	// initialize current & max values equal to the value of the first number in nums
+//     let current = nums[0];
+//     let max = nums[0];   
 	
-	// iterate through nums, starting at the second value
-    for(let i=1; i<nums.length; i++){      
+// 	// iterate through nums, starting at the second value
+//     for(let i=1; i<nums.length; i++){      
 		
-		// keep track of the current subArray, discarding and resetting if current is negative
-        current = Math.max(nums[i], current + nums[i]);
+// 		// keep track of the current subArray, discarding and resetting if current is negative
+//         current = Math.max(nums[i], current + nums[i]);
 		
-		// keep track of max subarray by checking to see if current subarray is greater than max
-        max = Math.max(current, max);
+// 		// keep track of max subarray by checking to see if current subarray is greater than max
+//         max = Math.max(current, max);
+//     }
+	
+//     return max;
+    let currentSum = nums[0], maxSum = nums[0]
+    
+	// Start from the second element 
+    for(let i = 1; i < nums.length; i ++){
+		// Check if the current sum is less than 0 to avoid negative sums
+		// If it is grater than 0 add it to the sum;
+        if(currentSum < 0) 
+            currentSum = nums[i];
+        else 
+            currentSum += nums[i];
+		// Always check for the max sum, if the current sum is greater, then replace the maxSum
+        if (currentSum > maxSum)
+            maxSum = currentSum;
     }
-	
-    return max;
+    return maxSum
 };
