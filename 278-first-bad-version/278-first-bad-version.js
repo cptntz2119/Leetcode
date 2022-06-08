8b331703ec24c0d7f6cb35d8ecd2ad7219dd1c1d
Dong <zhangdong2119@hotmail.com>
2022-06-08 16:09:39 -0700

    Time: 99 ms (24.62%), Space: 41.7 MB (69.53%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -17,12 +17,20 @@ var solution = function(isBadVersion) {
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
+    
     return function(n) {
-        let i = 0;
-        while(!isBadVersion(i)){
-            i++;
+        let left = 1;
+        let right = n;
+        while(left < right){
+            let mid = Math.floor(left+(right-left)/2);
+            if (isBadVersion(mid)){
+                right = mid
+            }else{
+                left = mid +1
+            }
+
         }
     
-        return i;
+        return left;
     };
 };
