795b80ace70b62e7d4c32affa9bc860049a1f2ef
Dong <zhangdong2119@hotmail.com>
2022-06-09 14:15:08 -0700

    Time: 101 ms (74.26%), Space: 48.6 MB (19.52%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -4,18 +4,10 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    let magArr = magazine.split('');
-let ranArr = ransomNote.split('');
-
-for (let i = 0; i < ranArr.length; i++) {
-  if(magArr.includes(ranArr[i])){
-    const magIndex = magArr.indexOf(ranArr[i]);
-    if (magIndex > -1) {
-      magArr.splice(magIndex, 1); // 2nd parameter means remove one item only
+    for (const letter of ransomNote) {
+        if (!magazine.includes(letter)) return false;
+        magazine = magazine.replace(letter, '');
     }
-  }else{
-    return false;
-  }
-}
-return true;
+    
+    return true;
 };
