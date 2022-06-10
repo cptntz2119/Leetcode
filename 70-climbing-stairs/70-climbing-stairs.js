57dd8b61ccd35f791cb113ad14624af904908377
Dong <zhangdong2119@hotmail.com>
2022-06-10 13:39:44 -0700

    Time: 67 ms (73.17%), Space: 42.1 MB (18.27%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    let f1 = 1, f2 = 2;
+    for (let i = 3; i <= n; i++) {
+        let tmp = f2 + f1;
+        f1 = f2;
+        f2 = tmp;
+    }
+    return f2;
+
+};
