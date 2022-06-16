fdeaf6af580bb02ee1970c98fa5800bc5e7dc54c
Dong <zhangdong2119@hotmail.com>
2022-06-15 19:03:11 -0700

    Time: 106 ms (35.52%), Space: 44.2 MB (45.44%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    // const map = {};
-    // for (const ele of nums){
-    //     if (!map[ele]){
-    //         map[ele] = 0;
-    //     }
-    //     map[ele]++;
-    // }
-    // for (const item in map){
-    //     if (map[item]>= nums.length/2){
-    //         return item
-    //     }
-    // }
+    const map = {};
+    for (const ele of nums){
+        if (!map[ele]){
+            map[ele] = 0;
+        }
+        map[ele]++;
+        if (map[ele]>= nums.length/2){
+            return ele;
+        }
     
-    nums.sort((a,b)=>a-b)
-     return nums[Math.floor(nums.length/2)]
+    }
+    
+        
+   //------------------- 
+    // nums.sort((a,b)=>a-b)
+    //  return nums[Math.floor(nums.length/2)]
 };
