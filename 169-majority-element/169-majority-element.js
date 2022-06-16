4a7d60670cbdc2e9446c13dcb18e45eda68136b2
Dong <zhangdong2119@hotmail.com>
2022-06-15 19:14:54 -0700

    Time: 81 ms (72.49%), Space: 44.2 MB (43.17%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,20 +3,34 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const map = {};
-    for (const ele of nums){
-        if (!map[ele]){
-            map[ele] = 0;
-        }
-        map[ele]++;
-        if (map[ele]>= nums.length/2){
-            return ele;
-        }
+    //-------------map
+//     const map = {};
+//     for (const ele of nums){
+//         if (!map[ele]){
+//             map[ele] = 0;
+//         }
+//         map[ele]++;
+//         if (map[ele]>= nums.length/2){
+//             return ele;
+//         }
     
-    }
+//     }
     
         
    //------------------- 
     // nums.sort((a,b)=>a-b)
     //  return nums[Math.floor(nums.length/2)]
+    
+    //----------Moore voting
+    
+    let count =0;
+    let candidate = null;
+    for (const num of nums){
+        if (count ===0){
+            candidate = num
+            
+        }
+        count += (num ===candidate) ? 1 : -1;
+    }
+    return candidate;
 };
