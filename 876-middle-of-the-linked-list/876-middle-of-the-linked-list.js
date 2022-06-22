05cee041da616e57870343bb2756ec1f41815d10
Dong <zhangdong2119@hotmail.com>
2022-06-21 18:12:32 -0700

    Time: 83 ms (49.29%), Space: 42 MB (52.77%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -10,19 +10,30 @@
  * @return {ListNode}
  */
 var middleNode = function(head) {
-    let current = head;
-    let length = 0;
-    let index = 0;
+//     let current = head;
+//     let length = 0;
+//     let index = 0;
     
-    while(current!==null){
-        length++;
-        current = current.next;
+//     while(current!==null){
+//         length++;
+//         current = current.next;
+//     }
+//     middle = Math.floor(length/2);
+//     current = head;
+//     while(index<middle){
+//         current = current.next;
+//         index++
+//     }
+//     return current;
+    //---------------------pointer
+    
+    if (head===null) return null;
+    let slow = head;
+    let fast = head;
+    
+    while (fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
     }
-    middle = Math.floor(length/2);
-    current = head;
-    while(index<middle){
-        current = current.next;
-        index++
-    }
-    return current;
+    return slow;
 };
