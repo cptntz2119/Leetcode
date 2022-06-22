da7b93ad52c412268441c91f23c466ec2f23af39
Dong <zhangdong2119@hotmail.com>
2022-06-21 18:30:21 -0700

    Time: 110 ms (29.62%), Space: 44.5 MB (95.97%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,8 +11,24 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0;
-    if(!root.left && !root.right) return 1;
+//     if (!root) return 0;
+//     if(!root.left && !root.right) return 1;
     
-    return Math.max(1+maxDepth(root.left), 1+maxDepth(root.right));
+//     return Math.max(1+maxDepth(root.left), 1+maxDepth(root.right));
+    if(!root) return 0;
+    let depth =0;
+    let queue = [root];
+    while(queue.length >0){
+        const children = [];
+        for (let node of queue){
+            if (!node) continue;
+            children.push(node.left);
+            children.push(node.right);
+        }
+        if (children.length >0){
+            depth ++;
+        }
+        queue = children;
+    }
+    return depth;
 };
