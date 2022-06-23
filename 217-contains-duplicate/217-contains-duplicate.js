a6be23937034d263f47394d780f0995ad3eead4d
Dong <zhangdong2119@hotmail.com>
2022-06-22 19:55:16 -0700

    Time: 218 ms (21.96%), Space: 50.8 MB (63.68%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,16 +3,20 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let map = {};
-    for (const item of nums){
-        if(map[item]){
-           return true;
-           }else{
-             map[item]=true;  
-           }
+//     let map = {};
+//     for (const item of nums){
+//         if(map[item]){
+//            return true;
+//            }else{
+//              map[item]=true;  
+//            }    
         
-        
-        
+//     }
+//     return false;
+    
+     nums.sort();
+    for(let i=0 ;i<nums.length-1; i++){
+        if(nums[i] == nums[i+1])    return true;
     }
     return false;
 };
