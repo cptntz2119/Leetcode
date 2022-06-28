687f29c22ac935eaee69bbd985ac19429eede8b8
Dong <zhangdong2119@hotmail.com>
2022-06-27 22:57:50 -0700

    Time: 282 ms (68.04%), Space: 57.9 MB (84.62%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,13 +3,35 @@
  * @param {number} k
  * @return {number[][]}
  */
-var kClosest = function(points, k) {
-    // Sort the array with a custom lambda comparator function
-    points.sort((a, b) => squaredDistance(a) - squaredDistance(b))
+// var kClosest = function(points, k) {
+//     // Sort the array with a custom lambda comparator function
+//     points.sort((a, b) => squaredDistance(a) - squaredDistance(b))
     
-    // Return the first k elements of the sorted array
-    return points.slice(0, k)
+//     // Return the first k elements of the sorted array
+//     return points.slice(0, k)
+// };
+
+// // Calculate and return the squared Euclidean distance
+// const squaredDistance = ([x, y]) => x ** 2 + y ** 2
+//-----------------
+var kClosest = function(points, k) {
+    let maxPQ = new MaxPriorityQueue()
+    for (let point of points) {
+        let dist = squaredDistance(point)
+        if (maxPQ.size() < k) {
+            // Fill the max PQ up to k points
+            maxPQ.enqueue(point, dist)
+        } else if (dist < maxPQ.front().priority) {
+            // If the max PQ is full and a closer point is found,
+            // discard the farthest point and add this one
+            maxPQ.dequeue()
+            maxPQ.enqueue(point, dist)
+        }
+    }
+    
+    // Return all points stored in the max PQ
+    return maxPQ.toArray().map(el => el.element)
 };
 
 // Calculate and return the squared Euclidean distance
-const squaredDistance = ([x, y]) => x ** 2 + y ** 2
+const squaredDistance = ([x,y]) => x ** 2 + y ** 2
