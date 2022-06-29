5b11a21cd0a7e55b955e0945f5b3360c84b23b14
Dong <zhangdong2119@hotmail.com>
2022-06-28 22:09:36 -0700

    Time: 128 ms (64.75%), Space: 46.7 MB (75.97%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -7,13 +7,13 @@ var lengthOfLongestSubstring = function(s) {
   left = 0,
   chars = new Set();
 
-  for (let i = 0; i < s.length; i++) {
-    while (chars.has(s[i])) {
+  for (let right = 0; right < s.length; right++) {
+    while (chars.has(s[right])) {
       chars.delete(s[left]);
       left++;
     }
 
-    chars.add(s[i]);
+    chars.add(s[right]);
     maxLength = Math.max(maxLength, chars.size);
   }
 
