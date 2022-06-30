646dbefc3858e665a6a9d0ca6ce5b963bb8e5545
Dong <zhangdong2119@hotmail.com>
2022-06-29 20:44:20 -0700

    Time: 89 ms (56.98%), Space: 43.3 MB (20.18%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -5,9 +5,23 @@
  */
 var twoSum = function(numbers, target) {
     
-  for(let start = 0, end = numbers.length-1; start < end;){
-        let sum = numbers[start] + numbers[end]
-        if(sum === target)return [++start, ++end]
-        else sum > target ? end-- : start++
+  // for(let start = 0, end = numbers.length-1; start < end;){
+  //       let sum = numbers[start] + numbers[end]
+  //       if(sum === target)return [++start, ++end]
+  //       else sum > target ? end-- : start++
+  //   }
+    
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum > target) {
+            right--;
+        } else {
+            left++;
+        }
     }
 };
