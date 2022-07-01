9b3005fd08205067b80830bb8315e32de12f3410
Dong <zhangdong2119@hotmail.com>
2022-06-30 20:10:17 -0700

    Time: 187 ms (73.50%), Space: 51.9 MB (92.62%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,37 +3,102 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b);    
-    let results = [];
-    for(let i = 0; i < nums.length; i++) {
+//     nums.sort((a, b) => a - b);    
+//     let results = [];
+//     for(let i = 0; i < nums.length; i++) {
+//         let j = i + 1;
+//         let k = nums.length - 1;
+        
+//         while(j < k) {
+//             const sum = nums[i] + nums[j] + nums[k];
+//             if (sum === 0) {
+//                 results.push([nums[i], nums[j] ,nums[k]]);
+                
+//                 while(j < k && nums[j] == nums[j+1]) {
+//                     j++;
+//                 }
+//                 j++;
+                
+//                 while(k > j && nums[k] == nums[k-1]) {
+//                     k--;
+//                 }
+//                 k--;
+//             } else if (sum < 0) {
+//                 j++;
+//             } else {
+//                 k--;
+//             }
+//         }
+        
+//         while(i < nums.length - 1 && nums[i] == nums[i+1]) {
+//             i++;
+//         }
+//     }    
+    
+//     return results;
+    
+    //--------------
+    const result = [];
+    const target = 0;
+    
+    // we need 3 values for this to work
+    // so return an empty array if we have less than 3
+    if (nums.length < 3) {
+        return result;
+    }
+    
+    // sorting is ok because the function is already O(n^2)
+    // and sort is O(nlogn)
+    // this also lets us stop iterating once weve passed the target value
+    nums = nums.sort((a,b) => a - b );
+    
+    // well use i as our anchor as we move through the array
+    // we stop at nums.length - 2 to prevent undefined for k
+    for (let i = 0; i < nums.length - 2; i++) {
+	
+        // because we sorted the array already
+        // we can stop here if the current iterator is greater than the target value
+        if (nums[i] > target) {
+            break;
+        }
+		
+        // if our iterator is the same as the previous value
+        // skip it to prevent duplicate results
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+		
+        // start j at i + 1
         let j = i + 1;
+		
+        // start k at end of array
         let k = nums.length - 1;
         
-        while(j < k) {
-            const sum = nums[i] + nums[j] + nums[k];
-            if (sum === 0) {
-                results.push([nums[i], nums[j] ,nums[k]]);
+        // walking j and k towards each other to find all possible values
+        // with i as our anchor value
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum === target) {
+                result.push([nums[i], nums[j], nums[k]]);
                 
-                while(j < k && nums[j] == nums[j+1]) {
-                    j++;
-                }
-                j++;
+                // skip duplicate values of j and k
+                while (nums[j] === nums[j + 1]) j++;
+                while (nums[k] === nums[k - 1]) k--;
                 
-                while(k > j && nums[k] == nums[k-1]) {
-                    k--;
-                }
-                k--;
-            } else if (sum < 0) {
+                // move j and k inward
                 j++;
-            } else {
                 k--;
+                continue;
+            }
+            if (sum < target) {
+                j++;
+                continue;
+            }
+            if (sum > target) {
+                k--;
+                continue;
             }
         }
-        
-        while(i < nums.length - 1 && nums[i] == nums[i+1]) {
-            i++;
-        }
-    }    
-    
-    return results;
+    }
+    return result;
 };
