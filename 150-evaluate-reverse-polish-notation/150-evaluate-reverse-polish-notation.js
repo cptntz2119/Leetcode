9021d50437ca53e02b8f110ed2df8986a9a4d2df
Dong <zhangdong2119@hotmail.com>
2022-07-05 21:09:48 -0700

    Time: 107 ms (48.90%), Space: 44.7 MB (75.92%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -2,16 +2,36 @@
  * @param {string[]} tokens
  * @return {number}
  */
-let a, b
-const evaluate = {"+": ()=>a+b, "-": ()=>a-b, "*": ()=>a*b, "/": ()=>~~(a/b)}
+// let a, b
+// const evaluate = {"+": ()=>a+b, "-": ()=>a-b, "*": ()=>a*b, "/": ()=>~~(a/b)}
 
-var evalRPN = function(tokens) {
-    let stack = []
-    for (let t of tokens) {
-        if (evaluate[t]) {
-            b = stack.pop(), a = stack.pop()
-            stack.push(evaluate[t]())
-        } else stack.push(~~t)
+// var evalRPN = function(tokens) {
+//     let stack = []
+//     for (let t of tokens) {
+//         if (evaluate[t]) {
+//             b = stack.pop(), a = stack.pop()
+//             stack.push(evaluate[t]())
+//         } else stack.push(~~t)
+//     }
+//     return stack[0]
+// };
+
+function evalRPN(tokens) {
+  let stack = [];
+  let ops = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b),
+  };
+  for (let t of tokens) {
+    if (ops[t]) {
+      let top = stack.pop();
+      let second = stack.pop();
+      stack.push(ops[t](second, top));
+    } else {
+      stack.push(Number(t));
     }
-    return stack[0]
+  }
+  return stack.pop();
 };
