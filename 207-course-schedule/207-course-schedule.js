8b52890d9cc128fddd5aace8bc4a64720412fa5d
Dong <zhangdong2119@hotmail.com>
2022-07-07 21:45:05 -0700

    Time: 112 ms (63.00%), Space: 47.1 MB (65.72%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -4,45 +4,33 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-  const indegree = new Array(numCourses).fill(0);
-  const queue = [];
-  /**
-   * The goal is to find whether the course graph has cycles.
-   * 
-   * We are looking for the number of indgree for each course and 
-   * put the course with no indegree into the queue. As we go 
-   * through the courses in queue, we break off the dependency(edge)
-   * from the current course in queue in all the prerequisite 
-   * pairs. Then we put all the courses with zero indegree into
-   * the queue. Repeat until the queue is empty. We maintain a 
-   * count and increment it each time we pop the queue. The count 
-   * will equal to the number of courses when there's no cycle and it 
-   * is possible to take all the courses.
-   *
-   * directed graph denotes: [prereq] --> [course] 
-   */
-  for (const [course, prereq] of prerequisites) {
-    indegree[course] += 1;
-  }
-  for (let i = 0; i < indegree.length; i++) {
-    if (indegree[i] === 0) {
-      queue.push(i);
-    } 
-  }
-  let count = 0;
-  
-  while (queue.length !== 0) { 
-    const c = queue.pop();
-    count += 1;
-    
-    for (const [course, prereq] of prerequisites) {
-      if (prereq === c) {
-        indegree[course] -= 1;
-        if (indegree[course] === 0) {
-          queue.push(course);
-        }
-      }
-    }
-  }
-  return count === numCourses;
+  const graph = Array.from({ length: numCourses }, () => []); // precourse: [course]
+	const indegree = Array.from({ length: numCourses }, () => 0); // course: dependency count
+
+	for (const [x, y] of prerequisites) {
+		graph[y].push(x); // y is requirement of x
+		indegree[x]++;
+	}
+
+	const queue = [];
+	let count = 0;
+
+	// add starting nodes to queue
+	for (let i = 0; i < indegree.length; i++) {
+		if (indegree[i] === 0) queue.push(i);
+	}
+
+	while (queue.length) {
+		const x = queue.shift();
+
+		count++;
+		for (const y of graph[x]) { // traverse adjacents
+			indegree[y]--; // decrement visited
+
+			// add nodes with indegree 0 to queue
+			if (!indegree[y]) queue.push(y);
+		}
+	}
+
+	return count === numCourses;
 };
