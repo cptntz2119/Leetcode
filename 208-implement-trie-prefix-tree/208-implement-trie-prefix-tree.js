58884d01e1d11aaeb6b3ff69845d6040f0d233b4
Dong <zhangdong2119@hotmail.com>
2022-07-08 20:22:43 -0700

    Time: 260 ms (66.07%), Space: 58.9 MB (78.76%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
@@ -1,13 +1,60 @@
 
-var Trie = function() {
-    this.map = {}
-};
-Trie.prototype.insert = function(word) {
-    this.map[word] = true;
-};
-Trie.prototype.search = function(word) {
-    return this.map[word] ? true : false;
-};
-Trie.prototype.startsWith = function(prefix) {
-    return Object.keys(this.map).filter(x => x.startsWith(prefix)).length > 0;
-};
+// var Trie = function() {
+//     this.map = {}
+// };
+// Trie.prototype.insert = function(word) {
+//     this.map[word] = true;
+// };
+// Trie.prototype.search = function(word) {
+//     return this.map[word] ? true : false;
+// };
+// Trie.prototype.startsWith = function(prefix) {
+//     return Object.keys(this.map).filter(x => x.startsWith(prefix)).length > 0;
+// };
+
+class Trie {
+  constructor() {
+    this.root = {};
+  }
+
+  insert(word) {
+    let node = this.root; // starting from root
+
+	// add each character to trie
+    for (const char of word) {
+	
+	// if char DNE, add
+      if (!node[char]) node[char] = {};
+
+      node = node[char]; // move on no next char node
+    }
+
+    node.isEnd = true; // set isEnd true to mark that we reach end of current word
+  }
+
+  search(word) {
+    let node = this.root;
+
+    for (const char of word) {
+	// if char we are looking for DNE, return false
+      if (!node[char]) return false;
+
+      node = node[char]; // move on to next node
+    }
+
+    return node.isEnd || false; // if is a word, return true. Else, false
+  }
+
+  startsWith(prefix) {
+    let node = this.root;
+
+    for (const char of prefix) {
+	// if char DNE, invalid prefix. Does not start with given prefix
+      if (!node[char]) return false;
+
+      node = node[char]; // move on to next node
+    }
+
+    return true;
+  }
+}
