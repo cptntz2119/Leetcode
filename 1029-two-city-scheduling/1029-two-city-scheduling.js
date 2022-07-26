3ef736b9f33c29f581998d39fc5c23fdbf47fa5a
Dong <zhangdong2119@hotmail.com>
2022-07-25 17:31:59 -0700

    Time: 137 ms (7.20%), Space: 31 MB (100.00%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -16,6 +16,10 @@ var twoCitySchedCost = function(costs) {
     }
     return sum;
     
-    
+//     Sort the persons in the ascending order by price_A - price_B parameter, which indicates the company additional costs.
+
+// To minimise the costs, send n persons with the smallest price_A - price_B to the city A, and the others to the city B.
+
+
     
 };
