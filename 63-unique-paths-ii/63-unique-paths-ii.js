f4295e3b1febc0c087fd36ec03a09701e573f27c
Dong <zhangdong2119@hotmail.com>
2022-07-26 17:21:18 -0700

    Time: 69 ms (88.47%), Space: 42.2 MB (80.69%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.js
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -5,15 +5,48 @@
 var uniquePathsWithObstacles = function(OG) {
     
     
-    if (OG[0][0]) return 0
-    let m = OG.length, n = OG[0].length
-    let dp = Array.from({length: m}, el => new Uint32Array(n))
-    console.log(dp);
-    dp[0][0] = 1
-    for (let i = 0; i < m; i++)
-        for (let j = 0; j < n; j++)
-            if (OG[i][j] || (!i && !j)) continue
-            else dp[i][j] = (i ? dp[i-1][j] : 0) + (j ? dp[i][j-1] : 0)
-    return dp[m-1][n-1]
+    // if (OG[0][0]) return 0
+    // let m = OG.length, n = OG[0].length
+    // let dp = Array.from({length: m}, el => new Uint32Array(n))
+    // console.log(dp);
+    // dp[0][0] = 1
+    // for (let i = 0; i < m; i++)
+    //     for (let j = 0; j < n; j++)
+    //         if (OG[i][j] || (!i && !j)) continue
+    //         else dp[i][j] = (i ? dp[i-1][j] : 0) + (j ? dp[i][j-1] : 0)
+    // return dp[m-1][n-1]
+    
+    const m = OG.length, n = OG[0].length 
+    
+    if(OG[0][0] || OG[m - 1][n - 1]) return false
+    
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(i === 0 && j === 0) {
+                OG[i][j] = 1
+                continue
+            }
+            
+            if(OG[i][j] === 1) {
+                OG[i][j] = 0
+                continue
+            }
+            
+            if(i === 0) {
+                OG[i][j] = OG[i][j - 1]
+                continue
+            }
+            
+            if(j === 0) {
+                OG[i][j] = OG[i - 1][j]
+                continue
+            }
+            
+            OG[i][j] = OG[i - 1][j] + OG[i][j - 1]
+        }
+    }
+    
+    
+    return OG[m - 1][n - 1]
     
 };
