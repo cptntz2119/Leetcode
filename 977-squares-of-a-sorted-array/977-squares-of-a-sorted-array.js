3b6a471149e20260d5dde01a9ee9aa5c37fd1981
Dong <zhangdong2119@hotmail.com>
2022-08-03 17:39:41 -0700

    Time: 149 ms (59.09%), Space: 49.1 MB (16.90%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,10 +3,24 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums
-                 .map(ele => Math.pow(ele,2))
-                 .sort((a,b)=>a-b)
-
-                     
+    // return nums
+    //              .map(ele => Math.pow(ele,2))
+    //              .sort((a,b)=>a-b)
+        let left = 0;
+        let right = nums.length-1;
+    
+        const result = [];
+    
+       for (let i= nums.length-1; i>=0; i--){
+            if (Math.pow(nums[left],2) < Math.pow(nums[right], 2)){
+                result[i] = Math.pow(nums[right],2);
+                right--;
+            }else{
+                result[i] = Math.pow(nums[left],2);
+                left++;
+            }
+        }
+    
+          return result;           
                      
 };
