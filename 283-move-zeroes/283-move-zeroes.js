3280e80938b9342c43f586a3c4bfc6472de5e621
Dong <zhangdong2119@hotmail.com>
2022-08-08 20:30:48 -0700

    Time: 139 ms (51.76%), Space: 46.4 MB (77.16%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,16 +3,9 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-//     let sortedArr = nums.sort((a,b)=>a-b);
-//     console.log(sortedArr);
     
-//     for (let i=0; i< nums.length; i++){
-//         if (sortedArr[i]===0){
-//             sortedArr.push(sortedArr.shift(sortedArr[i]))
-//         }
-//     }
-//     return sortedArr;
-    // failed at input [0,1]
+    
+   
     
      var idx = 0;
   for (var i = 0; i < nums.length; i++) {
@@ -21,5 +14,6 @@ var moveZeroes = function(nums) {
       nums[i] = idx === i ? nums[i] : 0;
       idx++;
     }
+      //console.log(nums);
   }
 };
