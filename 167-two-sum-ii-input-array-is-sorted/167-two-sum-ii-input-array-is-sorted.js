d73fa8faab2d1de7834b7cdea8039bfb04767a82
Dong <zhangdong2119@hotmail.com>
2022-08-10 19:15:02 -0700

    Time: 87 ms (63.94%), Space: 42.6 MB (86.18%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -10,18 +10,29 @@ var twoSum = function(numbers, target) {
   //       if(sum === target)return [++start, ++end]
   //       else sum > target ? end-- : start++
   //   }
-    
-    let left = 0;
-    let right = numbers.length - 1;
+   //---------------------- 
+//     let left = 0;
+//     let right = numbers.length - 1;
 
-    while (left < right) {
-        let sum = numbers[left] + numbers[right];
-        if (sum === target) {
-            return [left + 1, right + 1];
-        } else if (sum > target) {
-            right--;
-        } else {
-            left++;
+//     while (left < right) {
+//         let sum = numbers[left] + numbers[right];
+//         if (sum === target) {
+//             return [left + 1, right + 1];
+//         } else if (sum > target) {
+//             right--;
+//         } else {
+//             left++;
+//         }
+//     }
+    
+    //---------------------
+    const map = {};
+    for (let i=0; i< numbers.length;i++){
+        const seen = map[target-numbers[i]]
+        if(seen !== undefined){
+            return [seen+1, i+1]
         }
+        
+        map[numbers[i]]=i;
     }
 };
