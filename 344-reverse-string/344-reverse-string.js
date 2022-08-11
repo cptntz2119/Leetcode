ee73dcf18e4d577a8435e7ec2d31e2a77221fded
Dong <zhangdong2119@hotmail.com>
2022-08-10 19:27:45 -0700

    Time: 114 ms (76.84%), Space: 48.8 MB (91.38%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,14 +3,22 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left=0;
-    let right= s.length-1;
+//     let left=0;
+//     let right= s.length-1;
     
-    while(left<right){
-        let temp=s[left];
-        s[left]=s[right];
-        s[right]=temp;
-        left++;
-        right--;
-    }
+//     while(left<right){
+//         let temp=s[left];
+//         s[left]=s[right];
+//         s[right]=temp;
+//         left++;
+//         right--;
+//     }
+    //--------------
+     let i = 0, j = s.length - 1;
+  while (i < j) {
+    // ES6 destructuring assignment
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
 };
