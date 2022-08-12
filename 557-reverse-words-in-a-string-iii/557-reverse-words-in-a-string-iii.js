57ee8f85606caa4d25f6e536ffc84703075b19fa
Dong <zhangdong2119@hotmail.com>
2022-08-11 21:01:43 -0700

    Time: 100 ms (67.81%), Space: 48.1 MB (84.21%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -5,6 +5,12 @@
 
 
 var reverseWords = function(s) {
-     return s.split(' ').map(w => w.split('').reverse().join('')).join(' ');
+     // return s.split(' ').map(w => w.split('').reverse().join('')).join(' ');
+    let result = [];
+    for (const item of s.split(' ')) {
+        result.push(item.split('').reverse().join(''))
+    }
+
+    return result.join(' ');
     
 };
