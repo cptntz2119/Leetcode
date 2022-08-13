b5bdd5fc79ac4e21670bd9be5d29661816f903d5
Dong <zhangdong2119@hotmail.com>
2022-08-12 19:22:25 -0700

    Time: 70 ms (98.51%), Space: 48.3 MB (75.61%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -6,11 +6,20 @@
 
 var reverseWords = function(s) {
      // return s.split(' ').map(w => w.split('').reverse().join('')).join(' ');
-    let result = [];
-    for (const item of s.split(' ')) {
-        result.push(item.split('').reverse().join(''))
-    }
-
-    return result.join(' ');
     
+    //---------------
+//     let result = [];
+//     for (const item of s.split(' ')) {
+//         result.push(item.split('').reverse().join(''))
+//     }
+
+//     return result.join(' ');
+    //-------------
+    let words = s.split(" ");
+    let result = [];
+    result = words.map((word)=>{ 
+        return ((word.split("")).reverse()).join("");
+       //str->arr->str
+    })
+    return result.join(" ");
 };
