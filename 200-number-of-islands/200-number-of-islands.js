4e470f8c70100373b3cc7316413c37cd4a0938f3
Dong <zhangdong2119@hotmail.com>
2022-09-28 11:55:44 -0700

    Time: 104 ms (80.74%), Space: 44.6 MB (90.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -4,7 +4,7 @@
  */
 var numIslands = function(grid) {
     let count = 0;
-    
+    //explore the grid once find a land and check its adjacent, connect all adjacent island as whole counted as 1
     for (let i = 0; i < grid.length; i++){
         for (let j = 0; j < grid[i].length; j++){
             if(grid[i][j] === '1'){
@@ -17,13 +17,13 @@ var numIslands = function(grid) {
     return count;
     
    function dfs(x, y, grid) {
-        
+        //out of boundaries
         if (x < 0 || y < 0 || x >= grid.length  
          || y >= grid[x].length || grid[x][y] === '0')  {
         return
     }
-        
-        grid[x][y] = '0';
+        //set island to water because been visited so won't visit again
+        grid[x][y] = '0'; 
         
         dfs(x+1,y,grid);
         dfs(x-1,y,grid);
