1c730ec1fc3b9e71966c78efcbc337cb0c57a514
Dong <zhangdong2119@hotmail.com>
2022-09-29 11:43:06 -0700

    Time: 126 ms (69.00%), Space: 46.3 MB (83.03%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -0,0 +1,42 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solve = function(board) {
+    for (let row = 0; row < board.length; row++){
+        for (let col = 0; col < board[row].length; col++){
+            if (board[row][col] === 'O' && (row === 0 || col === 0 || row === board.length-1 || col === board[row].length - 1)){
+                
+                dfs(row, col, board);
+            }
+        }
+    }
+    
+    for (let row = 0; row < board.length; row++){
+        for (let col = 0; col < board[row].length; col++){
+            if (board[row][col] === 'visited'){
+                board[row][col] = 'O';
+            }else {
+                board[row][col] = 'X';
+            }
+        }
+    }
+    
+    function dfs(x, y) {
+        if (x < 0 || y < 0 || x >= board.length || y >= board[x].length || board[x][y] === 'X' || board[x][y] === 'visited'){
+            return;
+        }
+        
+        board[x][y] = 'visited';
+        
+        dfs(x+1, y);
+        dfs(x-1, y);
+        dfs(x, y+1);
+        dfs(x, y-1);
+        
+        return;
+    }
+    
+    
+    
+};
