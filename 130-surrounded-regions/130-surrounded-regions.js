4561f9d18358b49079037e7b93a729786f9c036b
Dong <zhangdong2119@hotmail.com>
2022-09-29 11:50:10 -0700

    Time: 177 ms (24.10%), Space: 46.4 MB (76.47%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.js
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -3,6 +3,7 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
+    //find the boundary 'O' first and then marked as visited in dfs
     for (let row = 0; row < board.length; row++){
         for (let col = 0; col < board[row].length; col++){
             if (board[row][col] === 'O' && (row === 0 || col === 0 || row === board.length-1 || col === board[row].length - 1)){
@@ -34,7 +35,7 @@ var solve = function(board) {
         dfs(x, y+1);
         dfs(x, y-1);
         
-        return;
+        
     }
     
     
