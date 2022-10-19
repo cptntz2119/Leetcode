52c33647edb1ce77c35be9d746536cee40eecb84
Dong <zhangdong2119@hotmail.com>
2022-10-18 19:43:22 -0700

    Time: 68 ms (95.35%), Space: 44.3 MB (42.71%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.js
+++ 0052-n-queens-ii/0052-n-queens-ii.js
@@ -38,3 +38,19 @@ var totalNQueens = function(n) {
     computePosForRow(0);
     return count;
 };
+
+//backtracking template
+// def backtrack(candidate):
+//     if find_solution(candidate):
+//         output(candidate)
+//         return
+    
+//     # iterate all possible candidates.
+//     for next_candidate in list_of_candidates:
+//         if is_valid(next_candidate):
+//             # try this partial candidate solution
+//             place(next_candidate)
+//             # given the candidate, explore further.
+//             backtrack(next_candidate)
+//             # backtrack
+//             remove(next_candidate)
