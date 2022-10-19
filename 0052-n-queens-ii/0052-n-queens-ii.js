37723125079b7e61f8b526675ee803605cd32fbe
Dong <zhangdong2119@hotmail.com>
2022-10-18 19:37:04 -0700

    Time: 113 ms (44.40%), Space: 44.3 MB (42.71%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.js
+++ 0052-n-queens-ii/0052-n-queens-ii.js
@@ -15,13 +15,17 @@ var totalNQueens = function(n) {
         }
         
         for (let col = 0; col < n; col++){
+            //jump over current position if queens on same col or diagnals
             if (cols.has(col) || posDiag.has(row + col) || negDiag.has(row - col)){
                 continue;
             }
             
             cols.add(col);
+            //positive diagnal row+col=constant
+            //negtive diagnals row-col=constant
             posDiag.add(row + col);
             negDiag.add(row - col);
+            
             computePosForRow(row + 1);
             
             cols.delete(col);
