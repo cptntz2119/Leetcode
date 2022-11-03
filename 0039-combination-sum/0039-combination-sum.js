6b2eaf32890cb979447f5a1c6df108b43bb4c936
Dong <zhangdong2119@hotmail.com>
2022-11-02 20:33:08 -0700

    Time: 163 ms (19.25%), Space: 48.8 MB (27.61%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -4,28 +4,53 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    let result = []; 
-    let temp = [];
+    const result = [];
+       
+    function backtrcking(temp = [], sum = 0, index =0){
+        if (sum > target) return;       
+        if (sum === target) result.push(temp);
+    
+        for (let i = index; i< candidates.length; i++) {
+            backtrcking([...temp, candidates[i]], sum+candidates[i], i); 
+        }
+    }
+        backtrcking();
+        return result;
    
     
-    backtracking(0, target);
-    return result;
+//     function permute(arr=[], sum=0, idx=0) {
+//         if(sum > target) return;
+//         if(sum === target) result.push(arr);
+        
+//         for(let i = idx; i < candidates.length; i++) {
+//             permute([...arr, candidates[i]], sum+candidates[i], i);
+//         }
+//     }
+//     permute()
+//     return result;
+        
+    
+//-------------------------------    
+//     let result = []; 
+//     let temp = [];
+   
+    
+//     backtracking(0, target);
+//     return result;
     
     
-    function backtracking(index, target){
-        if (target === 0) return result.push(temp.slice());
-        if (target < 0 ) return;
-        if (candidates.length === index) return;
+//     function backtracking(index, target){
+//         if (target === 0) return result.push(temp.slice());
+//         if (target < 0 ) return;
+//         if (candidates.length === index) return;
     
-        temp.push(candidates[index]);
-        backtracking(index, target - candidates[index]);
-        temp.pop();
-        backtracking(index+1, target);
+//         temp.push(candidates[index]);
+//         backtracking(index, target - candidates[index]);
+//         temp.pop();
+//         backtracking(index+1, target);
     
         
-    }
-    
-    
+//     }
     
     
     
